6254ad0a5ff2fc4470843e68a710fa3665351f54
Marco Holzer <marco.holzer1@gmail.com>
2022-10-03 13:48:37 +0200

    get_values_calendar now working

--- google_sheets_proj/get_values_sheet.py
+++ google_sheets_proj/get_values_sheet.py
@@ -7,10 +7,10 @@ scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name("google_sheets_proj\key_file.json", scope)
 client = gspread.authorize(creds)
 
-sheet = client.open("test").sheet1
+sheet = client.open("Arbeitszeit").sheet1
 #row_to_add = ["the", "first", "change"]
 #sheet.append_row(row_to_add)
 #sheet.delete_rows(7)
 
-data = sheet.get_all_records()
+data = sheet.col_values(1)
 pprint(data)
